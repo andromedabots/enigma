889b32f287f8843c3fb8b9f238364c20d3220a0b
Shadow <william.shadow@outlook.com>
2020-10-24 11:48:36 +0000

    item db setup

--- re/routes/eitem.js
+++ re/routes/eitem.js
@@ -4,6 +4,11 @@ const schema = new mongoose.Schema({
   name: { type: String, required: true },
   id: { type: String, required: true },
   type: { type: String, required: true },
+  // actions that the items do, only one is needed for an item
+  action: {
+    addShield: { type: Number },
+    log: {type: String} // console.log the value
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
