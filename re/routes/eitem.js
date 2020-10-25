691fe7612dfbd1aa137617246af11d4df6daef5b
Shadow <william.shadow@outlook.com>
2020-10-25 21:16:35 +0000

    oh yea i changed something before

--- re/routes/eitem.js
+++ re/routes/eitem.js
@@ -6,8 +6,12 @@ const schema = new mongoose.Schema({
   type: { type: String, required: true },
   // actions that the items do, only one is needed for an item
   action: {
-    addShield: { type: Number },
-    log: {type: String} // console.log the value
+    addShield: { type: Number, default: 0 }, // shield added to ship
+    addHealth: { type: Number, default: 0 }, // health added to ship
+    isWeapon: { type: Boolean, default: false}, // true if item is a weapon
+    isArtifact: { type: Boolean, default: false}, // true if item is an artifact
+    codeFile: {type: String}, // custom code file for an item
+    log: {type: String} // console.log the value for test items
   },
   createdAt: { type: Date, default: Date.now }
 });
