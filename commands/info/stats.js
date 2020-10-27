dc6b7ddd36ac00b1f881ac590570078ce2b8b66b
Shadow <william.shadow@outlook.com>
2020-10-27 09:18:18 +0000

    add stats command

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -1,21 +1,16 @@
 module.exports.run = async (client, message, args) => {
     const re = message.re
-    if(!args[0]) args[0] = message.author.id
     let user = re.fn.getuser(args.join(" "), message)
+    if(!user) user = re.fn.getuser(message.author.id, message)
     let data = await re.db.eusers.findOne({user: user.user.id}).exec()
     if(!data) return message.channel.send(`${user.user.tag} is not in the Enigma database yet!`)
+    if(data.location != message.author.euser.location) return message.channel.send("You can only see the stats of people in your current location!")
     let embed = new re.Discord.MessageEmbed()
-    .setTitle("Player Data for " + user.user.tag)
-    .setDescription(`ID: ${user.user.id}\nCreated at ${user.user.createdTimestamp}`)
-    .addField("Location", data.location, true)
-    .addField("Currently Traveling?", data.travel, true)
+    .setTitle(user.user.tag + "'s Enigma Stats")
+    .addField("Location", re.func.capitalizeFirstLetter(data.location), true)
     .addField("Balance", `${data.balance} EC`, true)
     .addField("Active Ship", data.ship, true)
     .addField("Ship Health", data.inventory.ships.find(x => x.id === data.ship).health, true)
-    let bp = await re.fn.botperms(user.user.id, message)
-    embed.addField("Permission Level", `${bp.level}: ${re.vars.botperms[bp.level]}`, true)
-    let items = `${data.inventory.items.map(x => `${x.id}${x.custom ? " (Custom Item)" : ""}`).join(`\n`)}`
-    embed.addField("Items:", `\`\`\`\n${items ? `${items}` : "No items in inventory"}\n\`\`\``)
 
     embed.setColor(re.config.color).setFooter("Information accurate as of").setTimestamp().setThumbnail(user.user.avatarURL({dynamic: true}))
     message.channel.send(embed)
@@ -23,7 +18,7 @@ module.exports.run = async (client, message, args) => {
   
   module.exports.help = {
     name:`${__filename.split(`${__dirname}/`).pop().split(`.`).shift()}`,
-    description:`See the data for a player`,
+    description:`See the stats for a player`,
     syntax:`${__filename.split(`${__dirname}/`).pop().split(`.`).shift()} <user>`,
     alias:[],
     module:`${__dirname.split(`/`).pop()}`,
