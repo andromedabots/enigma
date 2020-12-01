101a31cdade985d2fff037c2733531725fbf94e5
Shadow <william.shadow@outlook.com>
2020-12-01 23:48:58 +0000

    add cron filtering

--- commands/botadmin/cron.js
+++ commands/botadmin/cron.js
@@ -1,6 +1,11 @@
 module.exports.run = async (client, message, args) => {
     const re = message.re
-    let msg = await re.cron.run(args ? args : re.cron.list)
+    let run = []
+    args.forEach(x => {
+        if(re.cron.list.includes(x)) run.push(x)
+    })
+    if(run.length == 0 && args.length > 0) return message.channel.send("No vaild cron tasks found")
+    let msg = await re.cron.run(run.length > 0 ? run : re.cron.list)
     message.channel.send(msg)
     re.cron.cronlog(`${msg} - Manually triggered by ${message.author.tag}`)
 }
