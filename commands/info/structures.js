cefe25e0c1cb3455bc965c86f89935bcae7944cf
YoJames2019 <yobiscuit0@gmail.com>
2020-12-02 03:59:44 +0000

    improved the structures command

--- commands/info/structures.js
+++ commands/info/structures.js
@@ -1,34 +1,46 @@
 module.exports.run = async (client, message, args) => {
     const re = message.re
     let l = message.author.euser.location
-    let xl = await re.db.estructure.find({ system: l }).exec()
-    console.log(xl)
-    let embeds = [new re.Discord.MessageEmbed().setDescription("** **\n")]
-
-    for (var [i, structure] of xl.entries()) {
-        if (i % 10 == 0 && !i == 0) embeds.push(new re.Discord.MessageEmbed().setDescription("** **\n"))
-        //embeds[embeds.length - 1].description += `<@${x}> - ${re.client.users.cache.get(x) ? re.client.users.cache.get(x).username : "Unknown User"}\n`
-        let ft1 = structure.type.split("_"), ft2 = []
-        ft1.forEach(x => ft2.push(re.func.capitalizeFirstLetter(x)))
-
-        // when you move this command to the new UI, just leave this check like this (:
-        if(structure.type == "asteroid"){
-            embeds[embeds.length - 1].description += `**${structure.name} | ${ft2.join(" ")}**\n`
-        } else {
-            embeds[embeds.length - 1].description += `**${structure.name} | ${ft2.join(" ")}**\n`
+    let alllocations = await re.db.estructure.find({ system: l }).exec()
+    let embeds = [{ description: `** **\n`, fields:[] }]
+    let asteroids = {}
+    re.config.materials.forEach(x => asteroids[x] = [])
+    //sigh
+    for (let [i, structure] of alllocations.entries()) {
+        if (i % 10 == 0 && !i == 0) embeds.push({ description: `** **\n` })
+        let structurename = structure.type.split("_").map(x => re.func.capitalizeFirstLetter(x))
+        switch (structure.type) { 
+            case "asteroid":
+                if (!asteroids[structure.material]) asteroids[structure.material] = [];
+                asteroids[structure.material].push(structure.id)
+                break;
+            default:
+                embeds[embeds.length - 1].description += `**${structure.name} | ${structurename.join(" ")}**\n`
         }
     }
-
-    for (var [i, embed] of embeds.entries()) {
-        if (!embed.description) embed.description = "No structures found!"
-        embed.setTitle(`Structures in ${re.func.capitalizeFirstLetter(l)}`)
-            .setColor(re.config.color)
-            .setFooter(`Page ${i + 1}/${embeds.length} | Information accurate as of`)
-            .setTimestamp()
-        embed.description += "\n\n\n** **"
+    let asteroidslist = "";
+    for (let materials in asteroids) {
+        if (asteroids[materials].length > 0) asteroidslist += `${asteroids[materials].length} ${re.fn.capitalizeFirstLetter(materials)} Asteroid${asteroids[materials].length == 1 ? "" : "s"}\n`
     }
 
-    let m = await message.channel.send(embeds[0])
+    if (asteroidslist.length > 0) embeds[0].fields.push({ name: "Asteroids:", value: asteroidslist })
+
+    for (let [i, embed] of embeds.entries()) {
+        if (!embed.description || embed.description == "** **\n") embed.description = `No ${asteroidslist.length > 0 ? "other " : ""}structures found!`
+        embeds[i] = {
+            title: `Structures in ${re.func.capitalizeFirstLetter(l)}`,
+            color: re.config.color,
+            description: embed.description,
+            fields: embed.fields,
+            footer: {
+                text: `Page ${i + 1}/${embeds.length} | Information accurate as of`
+            },
+            timestamp: new Date()
+        }
+        embeds[i].description += "\n\n** **"
+    }
+
+    let m = await message.channel.send({ embed: embeds[0] })
     re.fn.paginator(message.author.id, m, embeds, 0)
 };
 
